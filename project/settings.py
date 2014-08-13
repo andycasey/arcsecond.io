4afb0970772134697932b2c571082a4fb2a1f3ae
Cédric Foellmi <cedric@onekilopars.ec>
2014-08-13 12:53:56 +0200

    Updating settings to include rest_framework static files.

--- project/settings.py
+++ project/settings.py
@@ -76,9 +76,7 @@ STATIC_URL = '/static/'
 STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
 STATICFILES_DIRS = (
     STATIC_PATH,
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, '../venv/lib/python2.7/site-packages/rest_framework/static/rest_framework'),
 )
 
 # List of finder classes that know how to find static files in
