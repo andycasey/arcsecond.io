8e4274c4bf32c223a56d198171a9d8a7c8ce75d7
Cédric Foellmi <cedric@onekilopars.ec>
2015-05-30 18:57:15 +0200

    Commenting out problematic line.

--- project/iobserve/models/accounts.py
+++ project/iobserve/models/accounts.py
@@ -1,17 +1,19 @@
 
 from django.db import models
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 from django.conf import settings
-
+#
 class UserProfile(models.Model):
     """
     Represent all the information associated with a user/author that must be added to the
     basic :model:`auth.User` class, which is kept as is for auth purposes only.
     """
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="profile")
+    class Meta: app_label = 'iobserve'
 
-    def __unicode__(self):
-        try:
-            return "UserProfile (user: "+self.user.get_username()+")"
-        except get_user_model().DoesNotExist:
-            return "UserProfile (no user set)"
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="profile", primary_key=True)
+
+    # def __unicode__(self):
+#         try:
+#             return "UserProfile (user: "+self.user.get_username()+")"
+#         except get_user_model().DoesNotExist:
+#             return "UserProfile (no user set)"
