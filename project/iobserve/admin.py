1e48d97b962727b97431942a7eb50c9d230cbde5
Cédric Foellmi <cedric@onekilopars.ec>
2015-05-31 12:00:23 +0200

    Better admin site

--- project/iobserve/admin.py
+++ project/iobserve/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.sites.admin import SiteAdmin
+from django.contrib.sites.models import Site
 
 # Register your models here.
+
+class WebSiteAdmin(SiteAdmin):
+    list_display = ("pk", "domain", "name")
+
+admin.site.unregister(Site)
+admin.site.register(Site, WebSiteAdmin)
+
