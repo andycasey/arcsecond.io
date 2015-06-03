55e90dc96788fb5667a83578ac31ece43790451a
Cédric Foellmi <cedric@onekilopars.ec>
2015-06-03 21:24:14 +0200

    Improving models

--- project/iobserve/models/common.py
+++ project/iobserve/models/common.py
@@ -1,19 +1,21 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
+bibcode_regex = "^[0-9]{4}[A-Za-z].{12}[0-9][A-Z]$"
 
 class Person(models.Model):
     class Meta: app_label = 'iobserve'
 
-    first_name = models.CharField(max_length=1000)
-    middle_name = models.CharField(max_length=1000)
-    last_name = models.CharField(max_length=1000)
+    first_name = models.CharField(max_length=1000, default="")
+    middle_name = models.CharField(max_length=1000, default="")
+    last_name = models.CharField(max_length=1000, default="")
 
 
 class BibliographicReference(models.Model):
     class Meta: app_label = 'iobserve'
 
-    title = models.CharField(max_length=1000)
+    title = models.CharField(max_length=1000, default="")
     year = models.IntegerField(default=0)
-
-# bibcode = models.RegexField(regex="[0-9]{4}[A-Za-z].{12}[0-9][A-Z]")
-#  authors = models.ManyToManyField(Person, related_name="authors")
+    bibcode = models.CharField(max_length=18, default="", validators=[RegexValidator(regex=bibcode_regex, message='Invalid bibcode', code='nomatch')])
+    authors = models.ManyToManyField(Person, related_name="authors")
 
