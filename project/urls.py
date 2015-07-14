59cb0995cb059855a1188b662745941f2c067c73
Cédric Foellmi <cedric@onekilopars.ec>
2015-07-14 07:58:58 +0200

    urls tweaks

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,11 @@
 
-from django.conf.urls import patterns
-from django.conf.urls import url
-from django.conf.urls import include
-
+from django.conf.urls import patterns, url, include
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^', include('project.iobserve.urls_www', namespace="www")),
+    url(r'^', include('project.iobserve.urls', namespace="www")),
     url(r'^', include('project.iobserve.urls_api', namespace="api")),
     url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('allauth.urls')),
