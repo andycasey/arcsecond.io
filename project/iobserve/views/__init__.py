9bd6fde407ea260a4f8ce8af74fa5c6e0a3e8b6f
Cédric Foellmi <cedric@onekilopars.ec>
2015-07-14 08:05:43 +0200

    Fixing some sky urls issues

--- project/iobserve/views/__init__.py
+++ project/iobserve/views/__init__.py
@@ -1,4 +1,4 @@
 from .pages import *
 from .earth import *
-# from .sky import *
+from .sky import *
 # from .users import *
--- project/iobserve/views/sky.py
+++ project/iobserve/views/sky.py
@@ -1,17 +1,18 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from ..connectors import *
-from ..serializers import *
-from ..models import Messages
+from project.iobserve import connectors
+from project.iobserve import serializers
+from project.iobserve import models
 
 
 @api_view(['GET'])
 def astronomical_object(request, name="."):
-    obj, created = AstronomicalObject.objects.get_or_create(name=name)
+    obj, created = models.AstronomicalObject.objects.get_or_create(name=name)
 
     if created or obj.coordinates is None:
-        coords, messages = get_SIMBAD_coordinates(name)
+        coords, messages = connectors.get_SIMBAD_coordinates(name)
 
         if coords is None:
             obj.messages = messages
@@ -20,47 +21,47 @@ def astronomical_object(request, name="."):
             obj.save()
 
         if created or obj.aliases.all().count() == 0:
-            aliases = get_SIMBAD_aliases(name)
+            aliases = connectors.get_SIMBAD_aliases(name)
             if len(aliases) > 0:
                 obj.aliases = aliases
                 obj.save()
 
         if created or obj.object_types.all().count() == 0:
-            otypes = get_SIMBAD_object_types(name)
+            otypes = connectors.get_SIMBAD_object_types(name)
             if len(otypes) > 0:
                 obj.object_types = otypes
                 obj.save()
 
         if created or obj.fluxes.all().count() == 0 or len([f for f in obj.fluxes.all() if not f.is_valid()]) > 0:
-            fluxes = get_SIMBAD_fluxes(name)
+            fluxes = connectors.get_SIMBAD_fluxes(name)
             if len(fluxes) > 0:
                 obj.fluxes = fluxes
                 obj.save()
 
     if hasattr(obj, "messages") is False:
-        obj.messages = Messages(http_status_code=200)
+        obj.messages = models.Messages(http_status_code=200)
 
-    serializer = AstronomicalObjectSerializer(obj)
+    serializer = serializers.AstronomicalObjectSerializer(obj)
     return Response(serializer.data)
 
 
 @api_view(['GET'])
 def astronomical_coordinates(request, name="."):
-    coords = get_SIMBAD_coordinates(name)
+    coords = connectors.get_SIMBAD_coordinates(name)
 
     if coords is None:
         return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
-    serializer = AstronomicalCoordinatesSerializer(coords)
+    serializer = serializers.AstronomicalCoordinatesSerializer(coords)
     return Response(serializer.data)
 
 
 @api_view(['GET'])
 def astronomical_object_aliases(request, name="."):
-    aliases = get_SIMBAD_aliases(name)
+    aliases = connectors.get_SIMBAD_aliases(name)
 
     if aliases is None:
         return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
-    serializer = AliasSerializer(aliases, many=True)
+    serializer = serializers.AliasSerializer(aliases, many=True)
     return Response(serializer.data)
