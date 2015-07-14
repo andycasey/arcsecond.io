854eb3d589ce09a90eec6e5dff4707d9bfa893f1
Cédric Foellmi <cedric@onekilopars.ec>
2015-07-14 22:03:16 +0200

    wrong urls?

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^', include('project.iobserve.urls', namespace="www")),
+    url(r'^', include('project.iobserve.urls_www', namespace="www")),
     url(r'^', include('project.iobserve.urls_api', namespace="api")),
     url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('allauth.urls')),
