d40ed14ff5fa8c844043cad82555b9bbd04a58a1
Cédric Foellmi <cedric@onekilopars.ec>
2015-07-18 10:40:01 +0200

    Releasing objects!

--- project/arcsecond/views/pages.py
+++ project/arcsecond/views/pages.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 
 def index(request):
   context = RequestContext(request)
-  context_dict = {"title": ("arcsecond.io")}
+  context_dict = {"title": ("arcsecond.io"), 'api_version': '1'}
   return render_to_response('arcsecond/index.html', context_dict, context)
 
 def custom_404(request):
