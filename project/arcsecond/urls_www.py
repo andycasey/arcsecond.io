6bfe681f4821727229371e3ef6f2b0c1bdabd769
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-01 15:58:25 +0200

    Fixing robots.txt problem

--- project/arcsecond/urls_www.py
+++ project/arcsecond/urls_www.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
-
-from rest_framework.urlpatterns import format_suffix_patterns
-from models import constants
+from django.views.generic import TemplateView
 
 from project.arcsecond import views
 
@@ -11,14 +9,13 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='arcsecond/robots.txt', mimetype='text/plain')),
     url(r'^observingsites/?$', views.observingsites, name="observingsites"),
 )
 
 if settings.DEBUG:
     import urls_api
     urlpatterns += urls_api.urlpatterns
-
-urlpatterns += format_suffix_patterns(urlpatterns)
 
 handler404 = 'views.custom_404'
 
