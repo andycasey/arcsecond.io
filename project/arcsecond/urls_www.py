f404e8444a9f2921d4b17b606f4a08da1b915630
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-04 10:13:17 +0200

    Fixing redirect and robots for staging / prod

--- project/arcsecond/urls_www.py
+++ project/arcsecond/urls_www.py
@@ -2,14 +2,23 @@ from django.conf import settings
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
 from django.http import HttpResponse
+from django.views.generic import RedirectView
 
 from project.arcsecond import views
 
+if settings.SITE_ID == 3:
+    robots_content = ""
+else:
+    robots_content = "User-agent: *\nDisallow: /"
+
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^onekilopars.ec$', RedirectView.as_view(url="/", permanent=True)),
+
     url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^robots\.txt$', lambda r: HttpResponse("")),
+    url(r'^robots\.txt$', lambda r: HttpResponse(robots_content)),
+
     url(r'^observingsites/?$', views.observingsites, name="observingsites"),
 )
 
