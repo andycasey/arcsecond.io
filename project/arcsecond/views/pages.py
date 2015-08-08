5dd7b1187daf6adcf26c423d3207b982e70d74a1
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-08 21:28:31 +0200

    indentation

--- project/arcsecond/views/pages.py
+++ project/arcsecond/views/pages.py
@@ -3,9 +3,9 @@ from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 
 def index(request):
-  context = RequestContext(request)
-  context_dict = {"title": ("arcsecond.io"), 'api_version': '1', 'initial': True}
-  return render_to_response('arcsecond/index.html', context_dict, context)
+    context = RequestContext(request)
+    context_dict = {"title": ("arcsecond.io"), 'api_version': '1', 'initial': True}
+    return render_to_response('arcsecond/index.html', context_dict, context)
 
 def custom_404(request):
     return render(request, 'arcsecond/404.html')
