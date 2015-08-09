d0ba025b3ef9035d5ca5c65c2adc402ba544fcb8
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-09 15:44:01 +0200

    tweaks

--- project/static/arcsecond/js/observingsites-maps.js
+++ project/static/arcsecond/js/observingsites-maps.js
@@ -5,19 +5,14 @@
 /* Requires Google Maps API */
 
 function initialize() {
-  var myLatlng = new google.maps.LatLng(0.0, 0.0); // Change your location
-  var mapOptions = {
-    zoom: 2, // Change zoom value
-    scrollwheel: false, // Change to "true" to enable users scale map on scroll
-    center: myLatlng
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var myLatlng = new google.maps.LatLng(0.0, 0.0); // Change your location
+    var mapOptions = {
+        zoom: 2, // Change zoom value
+        scrollwheel: false, // Change to "true" to enable users scale map on scroll
+        center: myLatlng
+    };
 
-  //var marker = new google.maps.Marker({
-  //    position: myLatlng,
-  //    map: map,
-  //    title: 'Home of onekilopars.ec and arcsecond.io'
-  //});
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
