210cf0db1b281f3abc2594aafad0d4da626128a7
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-13 17:59:25 +0200

    Import cleaning

--- project/arcsecond/serializers.py
+++ project/arcsecond/serializers.py
@@ -1,6 +1,5 @@
-from django.conf import settings
+
 from rest_framework import serializers
-from rest_framework.reverse import reverse
 from .models import *
 
 ######################## Shorts ########################
