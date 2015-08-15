0a68ddb5cde7396ff2f97ff2a99234d6f958d207
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-15 11:35:30 +0200

    Improving templates of accounts, adding signals etc.

--- project/arcsecond/apps.py
+++ project/arcsecond/apps.py
@@ -3,8 +3,12 @@
 # Do not import any models here.
 
 from django.apps import AppConfig
+from allauth.account.signals import user_signed_up
+from .signals import *
 
-class EcliptisAppConfig(AppConfig):
+class ArcsecondIOAppConfig(AppConfig):
     name = 'project.arcsecond'
     verbose_name = "arcsecond.io"
 
+    def ready(self):
+        user_signed_up.connect(process_upon_user_signed_up, dispatch_uid="process_upon_user_signed_up")
---
+++ project/arcsecond/signals.py
@@ -0,0 +1,4 @@
+
+def process_upon_user_signed_up(sender, **kwargs):
+    print 'User signed up'
+
