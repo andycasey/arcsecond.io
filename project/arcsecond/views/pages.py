27d75b219d8a782081d307a6d7a5cd9db489ebb5
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-16 10:44:21 +0200

    Fixed user profile and settings, their access etc.

--- project/arcsecond/views/pages.py
+++ project/arcsecond/views/pages.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse_lazy, reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 
@@ -23,10 +26,30 @@ def user_account_profile(request):
 def user_profile(request, username):
     context = RequestContext(request)
     context_dict = {"title": ("arcsecond.io - User Profile")}
-    return render_to_response('arcsecond/user-profile.html', context_dict, context)
+    try:
+        user = get_user_model().objects.get(username=username)
+    except ObjectDoesNotExist:
+        context_dict['username'] = username
+        return render_to_response('arcsecond/no-user-profile.html', context_dict, context)
+    else:
+        context_dict['user'] = user
+        return render_to_response('arcsecond/user-profile.html', context_dict, context)
 
+
+@login_required
 def user_settings(request, username):
     context = RequestContext(request)
     context_dict = {"title": ("arcsecond.io - User Settings")}
-    return render_to_response('arcsecond/user-settings.html', context_dict, context)
+
+    try:
+        user = get_user_model().objects.get(username=username)
+    except ObjectDoesNotExist:
+        raise Http404
+    else:
+        if user == request.user:
+            context_dict['user'] = user
+            return render_to_response('arcsecond/user-settings.html', context_dict, context)
+        else:
+            return HttpResponse('Unauthorized', status=401)
+
 
