4ecc7b543d8a8c57357a690e6481801cc4bcff1b
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-18 21:43:11 +0200

    Playing with errors in conversion

--- project/arcsecond/serializers.py
+++ project/arcsecond/serializers.py
@@ -357,11 +357,18 @@ class ICRSCoordinatesSerializer(serializers.ModelSerializer):
 
 ######################## Conversions ########################
 
+class ErrorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Error
+        fields = ('code', 'message')
+
 class CoordinatesConversionSerializer(serializers.ModelSerializer):
     class Meta:
         model = CoordinatesConversion
-        fields = ('input_first_value', 'input_second_value', 'input_frame', 'CIRS', 'FK4', 'FK4NoETerms',
-                  'FK5', 'GCRS', 'Galactic', 'ICRS')
+        fields = ('input_first_value', 'input_second_value', 'input_frame', 'error',
+                  'CIRS', 'FK4', 'FK4NoETerms', 'FK5', 'GCRS', 'Galactic', 'ICRS')
+
+    error = ErrorSerializer(required=False)
 
     CIRS = CIRSCoordinatesSerializer(required=False)
     FK4 = FK4CoordinatesSerializer(required=False)
@@ -375,9 +382,10 @@ class CoordinatesConversionSerializer(serializers.ModelSerializer):
 class TimesConversionSerializer(serializers.ModelSerializer):
     class Meta:
         model = TimesConversion
-        fields = ('input_format', 'input_value', 'documentation_URL', 'byear', 'byear_str', 'cxcsec', 'datetime',
+        fields = ('input_format', 'input_value', 'documentation_URL', 'error', 'byear', 'byear_str', 'cxcsec', 'datetime',
                   'decimalyear', 'gps', 'iso', 'isot', 'jd', 'jyear', 'jyear_str', 'mjd', 'plot_date', 'unix', 'yday')
 
+    error = ErrorSerializer(required=False)
 
 ######################## Telegrams ########################
 
