7585e886acf7e9d06c530f9c117581e45dd7db03
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-22 18:58:02 +0200

    Making map a global-scope var

--- project/static/arcsecond/js/observingsites-maps.js
+++ project/static/arcsecond/js/observingsites-maps.js
@@ -12,7 +12,7 @@ function initialize() {
         center: myLatlng
     };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    window.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
