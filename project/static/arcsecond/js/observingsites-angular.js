d59d7afcca18fcc8b10a5034224cc402214c371b
Cédric Foellmi <cedric@onekilopars.ec>
2015-08-22 18:58:15 +0200

    Achieving a nice maps of observatories.

--- project/static/arcsecond/js/observingsites-angular.js
+++ project/static/arcsecond/js/observingsites-angular.js
@@ -28,6 +28,9 @@ app.controller('SitesListController', ['$scope', '$http',
         ];
 
         siteList.sites = {};
+        siteList.markers = {};
+        siteList.infowindows = {};
+
         for (var i = 0; i < siteList.continents.length; i++) {
             var continentName = siteList.continents[i].name;
             $http({
@@ -36,7 +39,39 @@ app.controller('SitesListController', ['$scope', '$http',
                 params: {continent: continentName}
             })
                 .then(function(response) {
-                    $scope.siteList.sites[response.config.params.continent] = response.data;
+                    var continent_name = response.config.params.continent;
+                    $scope.siteList.sites[continent_name] = response.data;
+
+                    if (map !== "undefined") {
+                        continent_sites = $scope.siteList.sites[continent_name];
+                        var continent_markers = [];
+                        var continent_infowindows = [];
+
+                        for (var j = 0; j < continent_sites.length; j++) {
+                            var site = continent_sites[j];
+                            var latlong = new google.maps.LatLng(site.coordinates.latitude,site.coordinates.longitude);
+
+                            continent_infowindows[j] = new google.maps.InfoWindow({
+                                content: '<h5>'+site.long_name+'</h5><br/>'+site.country
+                            });
+
+                            continent_markers[j] = new google.maps.Marker({
+                                position: latlong,
+                                title: site.name
+                            });
+                            continent_markers[j].setMap(map);
+
+                            (function(k) {
+                                continent_markers[j].addListener('click', function() {
+                                    continent_infowindows[k].open(map, continent_markers[k]);
+                                });
+                            })(j);
+                        }
+
+                        $scope.siteList.markers[continent_name] = continent_markers;
+                        $scope.siteList.infowindows[continent_name] = continent_infowindows;
+                    }
+
                 }, function(response) {
                     alert("error "+response.status);
                 });
