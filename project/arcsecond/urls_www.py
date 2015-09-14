eb73b3cde98345a58c99cf41cef094ecf25738c1
Cédric Foellmi <cedric@onekilopars.ec>
2015-09-14 08:40:40 +0200

    Small reorg of urls to make it working better, and handle URL comflicts.

--- project/arcsecond/urls_www.py
+++ project/arcsecond/urls_www.py
@@ -5,25 +5,24 @@ from django.http import HttpResponse
 
 from project.arcsecond import views
 
-if settings.SITE_ID == 3:
-    robots_content = ""
-else:
-    robots_content = "User-agent: *\nDisallow: /"
+robots_content = "" if settings.SITE_ID == 3 else "User-agent: *\nDisallow: /"
 
+# The request-based view for the strictly empty URL / of host 'www'.
 urlpatterns = patterns('',
     url(r'^$', views.index_www, name='index_www'),
 )
 
+# Because we can't have a STAGING sub-subdomain api.arcsecond-staging.herokuapp.com, we put /api behind.
 if settings.SITE_ID == 2:
     import urls_api
     urlpatterns += patterns('',
         url(r'^api/', include(urls_api.urlpatterns))
     )
 
-urlpatterns += patterns('',
-    url(r'^observingsites/map?$', views.observingsites_map, name="observingsites-map"),
-    url('^.+$', views.ObservingSitesIndexView.as_view(), name='index_observingsites'),
+# All remaining non-empty patterns of the 'www'. Order matters! Put the all-catching webapp in last position,
+# to ensure all other have a chance to be caught.
 
+urlpatterns += patterns('',
     url(r'^robots\.txt$', lambda r: HttpResponse(robots_content)),
     url(r'^admin/', include(admin.site.urls)),
 
@@ -32,6 +31,11 @@ urlpatterns += patterns('',
     url(r'^accounts/profile', views.user_account_profile, name='user-account-profile'),
     url(r'^@(?P<username>[\w@\.]+)/$', views.user_profile, name="user-profile"),
     url(r'^@(?P<username>[\w@\.]+)/settings$', views.user_settings, name="user-settings"),
+
+    # The 'old' Angular piece of code, not using (yet) the service.
+    url(r'^observingsites/map?$', views.observingsites_map, name="observingsites-map"),
+
+    url('^.+$', views.ObservingSitesIndexView.as_view(), name='index_observingsites'),
 )
 
 handler404 = 'views.custom_404'
