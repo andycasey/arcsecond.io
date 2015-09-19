0797a0b6f06ad1ba4e402881c137420b1e0f9b75
Cédric Foellmi <cedric@onekilopars.ec>
2015-09-19 15:24:02 +0200

    Observing site search!

--- project/static/webapp/javascripts/webapp.config.js
+++ project/static/webapp/javascripts/webapp.config.js
@@ -19,13 +19,3 @@
     }
 })();
 
-//
-//arcsecondApp.config(function($interpolateProvider, $httpProvider) {
-//    $interpolateProvider.startSymbol('[[');
-//    $interpolateProvider.endSymbol(']]');
-//
-//    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-//    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-//    $httpProvider.defaults.withCredentials = true;
-//});
-//
