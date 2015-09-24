9d5e3ae310b8d5857eadf96e9bebf1f835cdc383
Cédric Foellmi <cedric@onekilopars.ec>
2015-09-24 18:55:00 +0200

    Getting telescope and observing sites in archives table!

--- project/static/webapp/javascripts/layout/controllers/index.archives.controller.js
+++ project/static/webapp/javascripts/layout/controllers/index.archives.controller.js
@@ -5,13 +5,14 @@
         .module('webapp.layout.controllers')
         .controller('ArchivesIndexController', ArchivesIndexController);
 
-    ArchivesIndexController.$inject = ['$scope', '$interval', 'Archives', 'Times', 'Snackbar'];
+    ArchivesIndexController.$inject = ['$scope', '$http', '$interval', 'Archives', 'Times', 'Snackbar'];
 
-    function ArchivesIndexController($scope, $interval, Archives, Times, Snackbar) {
+    function ArchivesIndexController($scope, $http, $interval, Archives, Times, Snackbar) {
         var vm = this;
 
         vm.data_rows = undefined;
         vm.coordinates = undefined;
+        vm._mappings = {};
 
         var tick = function() {
             $scope.times = Times.all(vm.coordinates);
@@ -29,6 +30,19 @@
 
             function successFn(data, status, headers, config) {
                 vm.data_rows = data.data;
+
+                for (var i = 0; i < vm.data_rows.length; i++) {
+                    var data_row = vm.data_rows[i];
+                    if (data_row.telescope !== undefined) {
+                        var telescope = vm._mappings[data_row.instrument_name];
+                        if (telescope !== undefined) {
+                            data_row.telescope = telescope;
+                        }
+                        else {
+                            $http.get(data_row.telescope, {"data_row": data_row}).then(telescopeSuccessFn, telescopeErrorFn)
+                        }
+                    }
+                }
             }
 
             function errorFn(data, status, headers, config) {
@@ -37,13 +51,36 @@
             }
 
             function positionSuccessFn(position) {
-                console.log(position);
                 vm.coordinates = position.coords;
             }
 
             function positionErrorFn(error) {
                 Snackbar.error(error);
                 console.log(error);
+            }
+
+            function telescopeSuccessFn(response) {
+                var data_row = response.config.data_row;
+                data_row.telescope = response.data;
+                vm._mappings[data_row.instrument_name] = data_row.telescope;
+
+                var observing_site = vm._mappings[data_row.telescope.name];
+                if (observing_site !== undefined) {
+                    data_row.telescope.observing_site = observing_site;
+                }
+                else {
+                    $http.get(data_row.telescope.observing_site, {"telescope": data_row.telescope}).then(observingSiteSuccessFn, telescopeErrorFn)
+                }
+            }
+
+            function observingSiteSuccessFn(response) {
+                var telescope = response.config.telescope;
+                telescope.observing_site = response.data;
+                vm._mappings[telescope.name] = telescope.observing_site;
+            }
+
+            function telescopeErrorFn(response) {
+                console.log(response);
             }
         }
     }
