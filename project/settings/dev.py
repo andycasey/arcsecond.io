8a3beccc9e3d30ad0161985c4c75764881903a7f
Cédric Foellmi <cedric@onekilopars.ec>
2015-10-04 12:25:01 +0200

    tweak cors settings to try solving current issues in production.

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -11,9 +11,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 BROKER_POOL_LIMIT = 1
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
 
-# Amazing trick: http://stackoverflow.com/questions/7335680/subdomain-on-localhost
 ARCSECOND_API_ROOT_URL = "http://api.lvho.st:8000"
 PARENT_HOST = "lvho.st:8000"
 
