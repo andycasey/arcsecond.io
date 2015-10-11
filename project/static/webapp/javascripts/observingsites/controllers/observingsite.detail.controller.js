6062be0ec4e6905376fea5cd0385bc0da0dc2bd1
Cédric Foellmi <cedric@onekilopars.ec>
2015-10-11 15:18:31 +0200

    Making the edition click go through a check function that will trigger an alert if not authenticated!

--- project/static/webapp/javascripts/observingsites/controllers/observingsite.detail.controller.js
+++ project/static/webapp/javascripts/observingsites/controllers/observingsite.detail.controller.js
@@ -5,12 +5,13 @@
         .module('webapp.observingsites.controllers')
         .controller('ObservingSiteDetailController', ObservingSiteDetailController);
 
-    ObservingSiteDetailController.$inject = ['$scope', '$filter', 'ObservingSites', 'uiGmapGoogleMapApi'];
+    ObservingSiteDetailController.$inject = ['$scope', '$filter', 'ObservingSites', 'Authentication', 'uiGmapGoogleMapApi'];
 
-    function ObservingSiteDetailController($scope, $filter, ObservingSites, uiGmapGoogleMapApi) {
+    function ObservingSiteDetailController($scope, $filter, ObservingSites, Authentication, uiGmapGoogleMapApi) {
         var vm = this;
         vm.observingsitedetail = undefined;
 
+        $scope.showAuthenticationWarning = false;
         $scope.continents = ObservingSites.continents;
         $scope.showContinents = function() {
             if (vm.observingsitedetail === undefined) {
@@ -18,6 +19,19 @@
             }
             var selected = $filter('filter')($scope.continents, {'name': vm.observingsitedetail.continent});
             return (vm.observingsitedetail.continent && selected.length) ? selected[0].name : 'Undefined';
+        };
+
+        $scope.checkAndShow = function(sender, btnName) {
+            if (Authentication.isAuthenticated()) {
+                sender[btnName].$show();
+            }
+            else {
+                $scope.showAuthenticationWarning = true;
+            }
+        };
+
+        $scope.closeAlert = function(sender) {
+            $scope.showAuthenticationWarning = false;
         };
 
         activate();
