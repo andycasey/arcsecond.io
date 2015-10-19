b3f88419b3735583c3ab3994d3c4348b089a1a0b
Cédric Foellmi <cedric@onekilopars.ec>
2015-10-19 21:52:02 +0200

    same

--- project/arcsecond/views/auth.py
+++ project/arcsecond/views/auth.py
@@ -1,5 +1,9 @@
-from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+# from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from allauth.socialaccount.providers.twitter.views import TwitterOAuthAdapter
 from rest_auth.registration.views import SocialLoginView
 
-class FacebookLogin(SocialLoginView):
-    adapter_class = FacebookOAuth2Adapter
+# class FacebookLogin(SocialLoginView):
+#     adapter_class = FacebookOAuth2Adapter
+
+class TwitterLogin(SocialLoginView):
+    adapter_class = TwitterOAuthAdapter
