92f241234fa7e1ae97c11f71ca3ad52d08a67f16
Cédric Foellmi <cedric@onekilopars.ec>
2015-10-24 11:05:52 +0200

    Back to username.

--- project/static/webapp/javascripts/authentication/controllers/login.js
+++ project/static/webapp/javascripts/authentication/controllers/login.js
@@ -3,13 +3,13 @@
 
     angular.module('webapp.authentication.controllers')
         .controller('LoginCtrl', function ($scope, $location, djangoAuth, Validate) {
-            $scope.model = {'email': '', 'password': ''};
+            $scope.model = {'username': '', 'password': ''};
             $scope.complete = false;
             $scope.login = function (formData) {
                 $scope.errors = [];
                 Validate.form_validation(formData, $scope.errors);
                 if (!formData.$invalid) {
-                    djangoAuth.login($scope.model.email, $scope.model.password)
+                    djangoAuth.login($scope.model.username, $scope.model.password)
                         .then(function (data) {
                             // success case
                             $location.path("/");
