d3da470713838ef0a777ca92af4673bbd99c28c8
Cédric Foellmi <cedric@onekilopars.ec>
2015-10-31 09:57:42 +0100

    Better feeling with loading observing site detail

--- project/static/webapp/javascripts/layout/controllers/update.observingsite.controller.js
+++ project/static/webapp/javascripts/layout/controllers/update.observingsite.controller.js
@@ -10,20 +10,24 @@
     function ObservingSiteUpdateController($scope, $routeParams, ObservingSites, Snackbar) {
         var vm = this;
         vm.observingsitedetail = undefined;
-
+        
         activate();
 
         function activate() {
+            $scope.viewLoading = true;
+
             if ($routeParams.site_name !== "new" && $routeParams.site_name !== undefined) {
                 ObservingSites.get($routeParams.site_name).then(successFn, errorFn);
             }
 
             function successFn(response, status, headers, config) {
                 vm.observingsitedetail = response.data;
+                $scope.viewLoading = false;
             }
 
             function errorFn(response, status, headers, config) {
                 console.log(response);
+                $scope.viewLoading = false;
             }
         }
     }
